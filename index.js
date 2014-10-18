fd2b45950852b6b5c9d626978928886470c65f03
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-18 23:10:58 +0700

    update some func

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 var vframe = module.exports = require('./lib/vframe');
+var datasourceJuggler = require('loopback-datasource-juggler');
+
+
+/**
+ * Connector
+ */
+
+vframe.Connector = require('./lib/connectors/base-connector');
+vframe.Memory = require('./lib/connectors/memory');
+vframe.Mail = require('./lib/connectors/mail');
+vframe.Remote = require('vsoft-connector-remote');
+
+
+
+/**
+ * Type
+ */
+vframe.ValidationError = vframe.Model.ValidationError;
+
