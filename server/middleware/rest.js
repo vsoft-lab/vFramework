16e786fbd534a86549570f600297d13d064dc285
NgaNguyenDuy <kyo1508@gmail.com>
2014-12-16 10:20:10 +0700

    add get currentContext

--- server/middleware/rest.js
+++ server/middleware/rest.js
@@ -2,15 +2,11 @@
  * Module dependencies.
  */
 var vframe = require('../../lib/vframe');
-
-
+var async = require('async');
 /*!
  * Export the middleware.
  */
 module.exports = rest;
-
-
-
 /**
  * Expose models over REST.
  *
@@ -22,44 +18,36 @@ module.exports = rest;
  * @header vframe.rest()
  */
 function rest() {
-    var tokenParser = null;
-    
-    return function (req, res, next) {
-        
+    return function restApiHandler(req, res, next) {
         var app = req.app;
-        var handler = app.handler('rest');
-        
-        if(req.url === '/routes') {
-            res.send(handler.adapter.allRoutes());
-            
-        } else if(req.url === '/models') {
+        var restHandler = app.handler('rest');
+        if (req.url === '/routes') {
+            return res.send(restHandler.adapter.allRoutes());
+        } else if (req.url === '/models') {
             return res.send(app.remotes().toJSON());
-            
-        } else if (app.isAuthEnabled) {
-            
-
-            
-            if (!tokenParser) {
+        }
+        var preHandlers;
+        if (!preHandlers) {
+            preHandlers = [];
+            var remotingOptions = app.get('remoting') || {};
+            var contextOptions = remotingOptions.context;
+            if (contextOptions !== false) {
+                if (typeof contextOptions !== 'object')
+                    contextOptions = {};
+                preHandlers.push(vframe.context(contextOptions));
+            }
+            if (app.isAuthEnabled) {
                 // NOTE(bajtos) It would be better to search app.models for a model
                 // of type AccessToken instead of searching all loopback models.
                 // Unfortunately that's not supported now.
+                // Related discussions:
                 var AccessToken = vframe.getModelByType(vframe.AccessToken);
-                tokenParser = vframe.token({ model: AccessToken });
-                
-                // console.log(tokenParser);
-
+                preHandlers.push(vframe.token({ model: AccessToken }));
             }
-            
-            tokenParser(req, res, function(err) {
-                if (err) {
-                    next(err);
-                } else {
-                    handler(req, res, next);
-                }
-            });
-
-        } else {
-            handler(req, res, next);
         }
+        
+        async.eachSeries(preHandlers.concat(restHandler), function(handler, done) {
+            handler(req, res, done);
+        }, next);
     };
 }
